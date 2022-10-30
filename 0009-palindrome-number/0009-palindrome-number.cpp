88625dd2256594b5cead9924b4060964473452cd
PriyangshuJS <116666129+PriyangshuJS@users.noreply.github.com>
2022-10-30 20:49:25 +0530

    Time: 28 ms (51.26%), Space: 5.9 MB (74.33%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long rem, rev=0, num=x;
-        if(x<0)
-        {
-            return false;
+        long long rev =0,rem;
+        if(x < 0){
+        return false;
         }
-
+        
+        int num = x;
         while(x != 0) 
         {
             rem = x % 10;
             rev = rev * 10 + rem;
             x /= 10;
         }
-        if(rev==num)
-        {
+
+        if(num==rev){
             return true;
         }
-        else
-            return false;
+        return false;
     }
 };
