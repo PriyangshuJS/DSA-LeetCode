8e522c36292f5b482857fbc5d84392b52d68f867
Priyangshu Jyoti Saha <116666129+PriyangshuJS@users.noreply.github.com>
2023-01-23 09:43:08 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (39.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -3,7 +3,7 @@ public:
     int lastStoneWeight(vector<int>& stones) {
         int len =stones.size();    
         
-        for(int i=0; i!=len-1; i++)
+        for(int i=0; i<len-1; i++)
         {
             sort(stones.begin(), stones.end());
             
