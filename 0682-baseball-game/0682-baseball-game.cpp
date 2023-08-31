309d49d1aef279f61498b957958417f2e70ace24
Priyangshu Jyoti Saha <116666129+PriyangshuJS@users.noreply.github.com>
2023-08-31 22:47:39 +0530

    Time: 7 ms (19.04%), Space: 8.4 MB (54.23%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,64 +1,32 @@
 class Solution {
 public:
     int calPoints(vector<string>& operations) {
-        stack<int>st;
-        int sum=0;
-        for(int i=0;i<operations.size();i++){
-            if(operations[i]=="D"){
-                int x=st.top()*2;
-                st.push(x);
-                sum=sum+st.top();
+        stack<int> ans;
+        int result = 0;
+        for (int i = 0; i < operations.size(); i++) {
+            if (operations[i] == "+") {
+                int temp = ans.top();
+                ans.pop();
+                int temp2 = ans.top()+temp;
+                ans.push(temp);
+                ans.push(temp2);
+                result+=ans.top();
+            } 
+            else if (operations[i] == "D") {
+                int temp = ans.top() * 2;
+                ans.push(temp);
+                result+=ans.top();
+            } 
+            else if (operations[i] == "C") {
+                result-=ans.top();
+                ans.pop();
             }
-            else if(operations[i]=="C"){
-             sum=sum-st.top();
-             st.pop();
-            }
-            else if(operations[i]=="+"){
-                int temp=st.top();
-                st.pop();
-                int y=st.top()+temp;
-                st.push(temp);
-                st.push(y);
-                sum=sum+st.top();
 
-            }
-            else{
-                st.push(stoi(operations[i]));
-                sum=sum+st.top();
+            else {
+                ans.push(stoi(operations[i]));
+                result+=ans.top();
             }
         }
-     return sum;
+        return result;
     }
 };
-
-
-// class Solution {
-// public:
-//     int calPoints(vector<string>& operations) {
-//         stack<int> ans;
-//         int result = 0;
-//         for (int i = 0; i < operations.size(); i++) {
-//             if (operations[i] == "+") {
-//                 int temp = ans.top();
-//                 ans.pop();
-//                 temp += ans.top();
-//                 ans.push(temp);
-//                 result+=ans.top();
-//             } 
-//             else if (operations[i] == "D") {
-//                 int temp = ans.top() * 2;
-//                 ans.push(temp);
-//                 result+=ans.top();
-//             } 
-//             else if (operations[i] == "C") {
-//                 result-=ans.top();
-//                 ans.pop();
-//             }
-//             else {
-//                 ans.push(stoi(operations[i]));
-//                 result+=ans.top();
-//             }
-//         }
-//         return result;
-//     }
-// };
